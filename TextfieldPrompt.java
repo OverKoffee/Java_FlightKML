0682110ef2e7b6ab8c6b7ed0e94ef1c0856ddbdc
Dustin Jordan <redmaldeveloper@gmail.com>
2019-02-11 21:26:23 -0900

    first draft of txtfield prompt / listener toggle

--- TextfieldPrompt.java
+++ TextfieldPrompt.java
@@ -1,6 +1,50 @@
 /*
+Class created by Redmal on 2/11/2019.
 This class will create a input text prompt for textfields.
  */
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
 public class TextfieldPrompt {
+    private JTextField textField;
+    private Font promptFont;
+    private Font inputFont;
+    private String promptText;
+
+    // passing 1 or 2 to constructor for
+    // building the prompt text accordingly
+    public TextfieldPrompt(int whichCoords){
+        textField = new JTextField("");
+        promptFont = new Font("promptFont", 2, 12); // italics and plain
+        inputFont = new Font("inputFont",1,12); // bold and plain
+
+        if (whichCoords == 1)
+            promptText = "Enter first coords...";
+        else if (whichCoords == 2)
+            promptText = "Enter second coords...";
+
+        textField.setText(promptText);
+        textField.setFont(promptFont);
+
+        // add event listener to toggle prompt and input
+        // text in the textfield
+        textField.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e) {
+                if (textField.getText() == "" || textField.getText() == promptText){
+                    textField.setText("");
+                    textField.setFont(inputFont);
+                }
+            }
+            public void focusLost(FocusEvent e) {
+                if (textField.getText() == "" || textField.getText() == promptText){
+                    textField.setText(promptText);
+                    textField.setFont(promptFont);
+                }
+            }
+
+        });
+    }
 }
